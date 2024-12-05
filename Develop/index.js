92c1d42ebf9c30a444dbe73abbba39ff9afd0bbb
Paige <paige.renee395@gmail.com>
2024-12-04 20:43:38 -0600

    I created code to dynamically generate a readme file based on user input in the command line.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,60 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application✅
+import fs from 'fs';
+import inquirer from 'inquirer';
+import { generateMarkdown } from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an array of questions for user input✅
+const questions = [{
+    message: "Please enter the title of your README:",
+    name: "userTitle"
+},{
+    message: "Please enter the description of your README:",
+    name: "userDescription"
+},{
+    message: "Please provide installation instructions:",
+    name: "userInstall"
+},{
+    message: "Please provide usage information:",
+    name: "userUsage"
+},{
+    type: "list",
+    message: "Please select a license:",
+    name: "userLicense",
+    choices: ['MIT', 'Apache', 'BSD', 'GPL', 'LGPL', 'AGPL']
+},{
+    message: "Please add any collaborators, third party assets, tutorials, etc that contributed to this project:",
+    name: "userContributing"
+},{
+    message: "Please provide examples for how to run tests for your application:",
+    name: "userTests"
+},{
+    message: "Please enter your GitHub username:",
+    name: "userGitHub",
+},{
+    message: "Please enter your email:",
+    name: "userEmail"
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const generatedMarkdown = generateMarkdown(data); // Generate Markdown
+  
+    fs.writeFile(fileName, generatedMarkdown, (err) => {
+      if (err) throw err;
+      console.log('README.md file generated successfully!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+  .then(answers => {
+    writeToFile('README.md', answers); // Pass the answers to writeToFile
+  })
+  .catch(error => {
+    console.error('Error initializing app:', error);
+  });
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(userLicense) {
+  if (userLicense) {
+    return `## License Badge
+![badmath](https://img.shields.io/badge/License-${userLicense}-blue)`
+  }
+  else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(userLicense) {
+  if (userLicense === "MIT") {
+    return '[MIT License](https://opensource.org/license/mit)';
+  }
+  else if (userLicense === "Apache") {
+    return '[Apache License](https://www.apache.org/licenses/LICENSE-2.0)';
+  }
+  else if (userLicense === "BSD") {
+    return '[BSD License](https://opensource.org/license/bsd-3-clause)';
+  }
+  else if (userLicense === "GPL") {
+    return '[GPL License](https://www.gnu.org/licenses/gpl-3.0.en.html)';
+  }
+  else if (userLicense === "LGPL") {
+    return '[LGPL License](https://www.gnu.org/licenses/lgpl-3.0.en.html)';
+  }
+  else if (userLicense === "AGPL") {
+    return '[AGPL License](https://www.gnu.org/licenses/agpl-3.0.en.html)';
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(userLicense) {
+  if (userLicense) {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+${userLicense}`
+  }
+}
 
-`;
+// TODO: Create a function to generate markdown for README✅
+export function generateMarkdown(data) {
+  return `# ${data.userTitle}
+
+${renderLicenseBadge(data.userLicense)}
+
+## Description
+
+${data.userDescription}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.userInstall}
+
+## Usage
+
+${data.userUsage}
+
+${renderLicenseSection(data.userLicense)}
+${renderLicenseLink(data.userLicense)}
+
+## Contributing
+
+${data.userContributing}
+
+## Tests
+
+${data.userTests}
+
+## Questions
+
+You can contact me on GitHub: [${data.userGitHub}](https://github.com/${data.userGitHub}) or via email: ${data.userEmail}.`
 }
 
 export default generateMarkdown;
